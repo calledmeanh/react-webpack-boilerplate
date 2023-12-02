fdc7af5c17fdfe778dd29b1fb1ae47d349e6188b
Hoang Anh <calledmeanh@gmail.com>
2023-12-02 14:46:15 +0700

    react-styledcomponent-webpack-configuration

---
+++ README.md
@@ -0,0 +1,10 @@
+# react-webpack-boilerplate
+react-webpack-boilerplate
+
+## Use
+`yarn` or `npm install` then `yarn start` or `npm start`
+
+## Build
+`yarn build` or `npm run build`
+
+Happy coding!
---
+++ webpack.common.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+module.exports = {
+  entry: path.join(__dirname, 'src', 'index.js'),
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, include: path.join(__dirname, 'src'), use: ['babel-loader'] },
+      { test: /\.css$/i, use: [MiniCssExtractPlugin.loader, 'css-loader'] },
+      { test: /\.(png|svg|jpg|jpeg|gif)$/i, type: 'asset/resource' },
+    ],
+  },
+  optimization: {
+    minimizer: [new CssMinimizerPlugin()],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'dist', 'index.html'),
+    }),
+    new MiniCssExtractPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+};

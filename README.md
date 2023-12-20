57d22f5aa6355d36f3d60e015a93d2e040942f06
Hoang Anh <calledmeanh@gmail.com>
2023-12-20 21:01:22 +0700

    format by prettier confgi

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # react-webpack-boilerplate
+
 react-webpack-boilerplate
 
 ## Use
+
 `yarn` or `npm install` then `yarn start` or `npm start`
 
 ## Build
+
 `yarn build` or `npm run build`
 
 Happy coding!
--- webpack.common.js
+++ webpack.common.js
@@ -1,21 +1,21 @@
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = {
-  entry: path.join(__dirname, 'src', 'index.js'),
+  entry: path.join(__dirname, "src", "index.js"),
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
     clean: true,
   },
   module: {
     rules: [
-      { test: /\.js$/, exclude: /node_modules/, include: path.join(__dirname, 'src'), use: ['babel-loader'] },
-      { test: /\.css$/i, use: [MiniCssExtractPlugin.loader, 'css-loader'] },
-      { test: /\.(png|svg|jpg|jpeg|gif)$/i, type: 'asset/resource' },
+      { test: /\.js$/, exclude: /node_modules/, include: path.join(__dirname, "src"), use: ["babel-loader"] },
+      { test: /\.css$/i, use: [MiniCssExtractPlugin.loader, "css-loader"] },
+      { test: /\.(png|svg|jpg|jpeg|gif)$/i, type: "asset/resource" },
     ],
   },
   optimization: {
@@ -23,7 +23,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'dist', 'index.html'),
+      template: path.join(__dirname, "dist", "index.html"),
     }),
     new MiniCssExtractPlugin(),
     new webpack.HotModuleReplacementPlugin(),
